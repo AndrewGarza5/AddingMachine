e198ca4772ceb5aa4c22da88cb160802122e8a94
AndrewGarza5 <Garza.andrew005@yahoo.com>
2019-10-09 19:24:49 -0700

    Added comments

--- cse360assign2/src/cse360assign2/AddingMachine.java
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -1,29 +1,57 @@
-package cse360assign2;
+/*
+ * Name: Andrew Garza
+ * ID: 1213817694
+ * Seat: R A 8
+ * Assignment Number: 2
+ */
 
+package cse360assign2;
+/**
+ * AddingMachine class that does basic mathematical functions and has
+ * helper functions as well. Beyond the class itself, this project is intended
+ * for practicing with version control systems
+ * @author Andrew Garza
+ *
+ */
 public class AddingMachine {
 	
 	private int total;
-	
+	/**
+	 * Constructor setting the initial total equal to 0
+	 */
 	public AddingMachine () {
 		total = 0;  // not needed - included for clarity
 	}
-	
+	/**
+	 * returns the current total 
+	 * @return
+	 */
 	public int getTotal () {
 		return 0;
 	}
-	
+	/**
+	 * adds a given value to the current total 
+	 * @param value is the number the user wants to add
+	 */
 	public void add (int value) {
 		
 	}
-	
+	/**
+	 * subtracts a given value to the current total
+	 * @param value is the number the user wants to subtract
+	 */
 	public void subtract (int value) {
 		
 	}
-	
+	/**
+	 * Function that displays the history of adding and subtracting
+	 */
 	public String toString () {
 		return "";
 	}
-	
+	/**
+	 * resets the current total and everything
+	 */
 	public void clear() {
 		
 	}
